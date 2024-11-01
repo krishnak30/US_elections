e3ec74cb1fcb9c10f6808fbbbd603b69684a8047
Denise Chang <ddleletan@hotmail.com>
2024-11-01 18:11:33 -0400

    rename scripts

---
+++ scripts/03-test_analysis_data.R
@@ -0,0 +1,10 @@
+#### Preamble ####
+# Purpose: Tests the structure and validity of the analysis data set
+# Author: Krishna Kumar
+# Date: November 1 2024
+# Contact: krishna.kumar@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# - 00-install_packages.R
+# - 02-clean_data.R
+
